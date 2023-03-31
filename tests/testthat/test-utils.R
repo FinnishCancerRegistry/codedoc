0e673971b363827f263441edd9607f3c91f0b31e
Joonas Miettinen <joonas.miettinen@cancer.fi>
2023-03-31 11:17:42 +0300

    test: update string_interpolation unit test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,24 +1,14 @@
-
-
-
-
-
-
-
 testthat::test_that("string_interpolation works", {
 
   neo <- "mr. anderson"
-  testthat::expect_identical(
-    string_interpolation("hello ${neo}", environment()),
-    "hello mr. anderson"
+  obs <- string_interpolation(
+    "hello ${neo}",
+    environment(),
+    debug_data = list(
+      text_file_path = "[unknown text path]",
+      first_line_no = 1L
+    )
   )
-
+  exp <- "hello mr. anderson"
+  testthat::expect_identical(obs, exp)
 })
-
-
-
-
-
-
-
-
